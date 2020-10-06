5a1729b8098f142777a4f8efdb18b0b528f3c489
faelfer <rfapp00@gmail.com>
2020-10-05 21:29:11 -0300

    Lesson | Start a Working Express Server

--- myApp.js
+++ myApp.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+app.get('/', (req, res) => {
+  res.send('Hello Express')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`)
+})
