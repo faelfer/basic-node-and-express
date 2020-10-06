6e48cec4893f1488c558bd6a76216132c3732720
faelfer <rfapp00@gmail.com>
2020-10-05 22:21:09 -0300

    Lesson | Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
-app.get('/', (req, res) => {
-  res.send('Hello Express')
-})
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening at http://localhost:${process.env.PORT}`)
